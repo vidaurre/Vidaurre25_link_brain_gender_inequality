3c0fe0c8ea2e8fc82d43755672060cc75468384a
Diego Vidaurre <dvidaurre@gmail.com>
2025-11-20 06:51:41 +0100

    Delete run_pipeline.sh

--- run_pipeline.sh
+++
@@ -1,8 +0,0 @@
-python3.12 preprocess_socioeconomic_data.py3
-python3.12 preprocess_brain_data.py
-python3.12 gather_demographics.py
-python3.12 compute_between_sexes_distances.py
-python3.12 predict_sex.py
-python3.12 compute_shap_values.py
-python3.12 run_ftests.py
-python3.12 run_ttests.py
